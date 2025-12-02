bca6ee9361fe3d335e1f179d394345a7fe431408
EslamKamel89 <elsamkamelforex@gmail.com>
2025-12-02 08:41:49 +0200

    feat(store): add Category model and register app in settings

--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100 , null=False , unique=True , db_index=True)
+    slug = models.SlugField(max_length=255 , unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.name.capitalize()}"
+    class Meta :
+        verbose_name_plural = 'categories'
