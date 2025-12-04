67401c8fa7749a99fe683588a36c42efa45f437c
EslamKamel89 <elsamkamelforex@gmail.com>
2025-12-04 11:13:26 +0200

    feat(store): register Category and Product models in Django admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug", "created_at", "updated_at")
+    list_display_links = ("name", "slug")
+    list_filter = ("created_at",)
+    search_fields = ("name", "slug")
+    ordering = ("-created_at",)
+    prepopulated_fields = {"slug": ("name",)}
+    list_per_page = 25
+    date_hierarchy = "created_at"
+    # readonly_fields = ("id",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
