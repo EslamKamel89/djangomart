a6a8e8e215da829c81c9d30253bb110f139e9c4c
EslamKamel89 <elsamkamelforex@gmail.com>
2025-12-07 11:21:54 +0200

    feat(store): add product detail page and extract product card to partial

--- store/urls.py
+++ store/urls.py
@@ -2,4 +2,7 @@ from django.urls import URLPattern, path
 
 from . import views
 
-urlpatterns: list[URLPattern] = [path("", views.HomeView.as_view())]
+urlpatterns: list[URLPattern] = [
+    path("", views.HomeView.as_view()),
+    path("products/<slug:slug>", views.ProductView.as_view(), name="product-view"),
+]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
+from typing import Any
+
 from django.http import HttpRequest
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView, View
+from django.views.generic import DetailView, ListView, TemplateView, View
 
 from store.models import Category, Product
 
@@ -13,3 +15,18 @@ class HomeView(View):
     def get(self, request: HttpRequest):
         products = Product.objects.select_related("category").all()
         return render(request, "store/home.html", {"products": products})
+
+
+class ProductView(DetailView):
+    template_name = "store/product-details.html"
+    model = Product
+
+    def get_queryset(self):
+        return Product.objects.select_related("category")
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        data = super().get_context_data(**kwargs)
+        product: Product = self.get_object()  # type: ignore
+        data["related_products"] = Product.objects.filter(category=product.category)[2:]
+        print(data["related_products"])
+        return data
