fcfcde279fca649b992dd274e8ba416cc6eac089
EslamKamel89 <elsamkamelforex@gmail.com>
2025-12-08 06:26:54 +0200

    feat(store): add get_absolute_url and improve product detail UI

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -29,3 +30,6 @@ class Product(models.Model):
 
     def __str__(self) -> str:
         return f"{self.title} - (${self.price})"
+
+    def get_absolute_url(self):
+        return reverse("product-view", args=[self.slug])
