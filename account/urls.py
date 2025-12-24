513fb4430b230aaf9f4e927da49b71ef815eb42a
EslamKamel89 <elsamkamelforex@gmail.com>
2025-12-24 11:35:29 +0200

    fix(auth): move register route to /register and update links

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,5 @@ from django.urls import URLPattern, path
 from . import views
 
 urlpatterns: list[URLPattern] = [
-    path("", views.RegisterView.as_view(), name="register")
+    path("register", views.RegisterView.as_view(), name="register")
 ]
