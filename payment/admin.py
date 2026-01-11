27488d2bd36646bbfe18d774e2b9d9b43ce8142a
EslamKamel89 <elsamkamelforex@gmail.com>
2026-01-12 00:38:10 +0200

    feat(payment): add ShippingAddress model, migration, and admin configuration

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from payment.models import ShippingAddress
+
+
+@admin.register(ShippingAddress)
+class ShippingAddressAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "full_name",
+        "email",
+        "city",
+        "state",
+        "zipcode",
+        "user",
+    )
+    list_display_links = ("full_name", "email")
+    list_filter = ("city", "state")
+    search_fields = ("full_name", "email", "city", "state", "zipcode")
+    ordering = ("full_name", "email", "city", "state", "zipcode")
--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class ShippingAddress(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="shipping_addresses"
+    )
+    full_name = models.CharField(max_length=255, db_index=True)
+    email = models.EmailField(max_length=255, db_index=True)
+    address1 = models.TextField()
+    address2 = models.TextField(null=True, blank=True)
+    city = models.CharField(max_length=100, db_index=True)
+    state = models.CharField(max_length=100, null=True, blank=True, db_index=True)
+    zipcode = models.CharField(max_length=20, null=True, blank=True, db_index=True)
+
+    def __str__(self) -> str:
+        return f"{self.full_name} - {self.city}"
+
+    class Meta:
+        verbose_name_plural = "Shipping Addresses"
+        ordering = ["full_name"]
